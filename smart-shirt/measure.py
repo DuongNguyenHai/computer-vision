c284eaf7aefcb7a00277adddcead2c0f3a917b7f
DuongNguyenHai <duongnguyenhai.hust@gmail.com>
2018-01-08 23:19:59 +0700

    update

--- smart-shirt/measure.py
+++ smart-shirt/measure.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+import argparse
 import cv2 as cv
 import numpy as np
-import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "path to the image file")
@@ -15,7 +16,7 @@ cv.imshow('original', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', gray)
 # thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 1)
-(_, thresh) = cv.threshold(gray, 40, 255, cv.THRESH_BINARY)
+(_, thresh) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY_INV)
 cv.imshow('thresh', thresh)
 
 (_,conts,_) = cv.findContours(thresh,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -24,10 +25,26 @@ bigestCont = sorted(conts, key=cv.contourArea, reverse=True)[0]
 rect = cv.minAreaRect(bigestCont)
 print(rect[0])
 box = np.int0(cv.boxPoints(rect))
+hull = cv.convexHull(bigestCont)
 
-img = cv.drawContours(img, [bigestCont], -1, (220,220,0), 1)
-img = cv.drawContours(img, [box], -1, (0,255,0), 1)
+c = hull
+extLeft = tuple(c[c[:, :, 0].argmin()][0])
+extRight = tuple(c[c[:, :, 0].argmax()][0])
+extTop = tuple(c[c[:, :, 1].argmin()][0])
+extBot = tuple(c[c[:, :, 1].argmax()][0])
 
+# print(hull)
+# img = cv.drawContours(img, [bigestCont], -1, (220,220,0), 1)
+# img = cv.drawContours(img, [box], -1, (0,255,0), 1)
+# img = cv.drawContours(img, [hull], -1, (0,0,255), 2)
+
+# draw
+cv.circle(img, extLeft, 4, (0, 0, 255), -1)
+cv.circle(img, extRight, 4, (0, 255, 0), -1)
+cv.circle(img, extTop, 4, (255, 0, 0), -1)
+cv.circle(img, extBot, 4, (255, 255, 0), -1)
+
+# find center of shirt
 M = cv.moments(box)
 cx = int(M['m10']/M['m00'])
 cy = int(M['m01']/M['m00'])
@@ -36,8 +53,6 @@ cv.circle(img, (cx, cy), 3, (255, 255, 255), -1)
 cv.putText(img, '('+str(cx)+','+str(cy)+')', (cx + 20, cy), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
 cv.imshow('contour', img)
-
-
 
 cv.waitKey(0)
 cv.destroyAllWindows()
