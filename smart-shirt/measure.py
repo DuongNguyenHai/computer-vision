b042ce296a2327aa7337473259ef6f61925daeb3
DuongNguyenHai <duongnguyenhai.hust@gmail.com>
2018-01-11 00:03:35 +0700

    found 8 points

--- smart-shirt/measure.py
+++ smart-shirt/measure.py
@@ -2,7 +2,7 @@
 import argparse
 import numpy as np
 import cv2 as cv
-import shirtIP
+import shirtIP as Tshirt
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "path to the image file")
@@ -10,7 +10,7 @@ args = vars(ap.parse_args())
 
 img = cv.imread(args["image"])
 
-shirt = shirtIP.TShirt(args["image"])
+shirt = Tshirt.TShirt(args["image"])
 
 # auto detect all spcecial point of t-shirt
 shirt.recognize()
@@ -24,27 +24,49 @@ lst = sorted(arr, key=lambda x: x[1], reverse=False)
 lt = tuple(map(tuple, lst))
 # print(lt)
 print('center: ', shirt.ct)
+print('collar: ', shirt.collar)
 print('body bottom: ', shirt.bodyBottom)
+print('sleeveTop:', shirt.sleeveTop)
+print('sleeveBottom: ', shirt.sleeveBottom)
+print('arm hole: ', shirt.armHoleBottom)
 
-sleLT = shirt.getSleeveUpper(hull)
-sleRT = shirt.getSleeveUpper(hull, side='right')
+# sleLT = shirt.getsleeveTop(hull)
+# sleRT = shirt.getsleeveTop(hull, side='right')
 
-ll = shirt.outline[:,0].tolist()
-print(ll.index(list(shirt.bodyBottom[0])))
-print(ll.index(list(sleLT)))
+# ll = shirt.outline[:,0].tolist()
+# tail = ll.index(list(sleLT))
+# head = ll.index(list(shirt.bodyBottom[0]))
+# print(tail)
+# print(head)
+# print("collar left:",ll.index(list(shirt.collar[0])))
+# print("collar right:", ll.index(list(shirt.collar[1])))
 
+# cut = shirt.cutContour(shirt.outline, tail, head+1)
+# huCut = shirt.getHull(cut)
 
-img = cv.drawContours(img, [shirt.outline], -1, (220,220,0), 1)
+# img = cv.drawContours(img, [huCut], -1, (255,0,255), 1)
+# print(tuple(map(tuple,huCut[:,0])))
+# for point in huCut:
+#     cv.circle(img, tuple(point[0]), 2, (0, 0, 255), -1)
+
+img = cv.drawContours(img, [shirt.outline], -1, (220,220,220), 1)
 # img = cv.drawContours(img, [box], -1, (0,255,0), 1)
-img = cv.drawContours(img, [hull], -1, (255,0,255), 1)
+img = cv.drawContours(img, [shirt.hull], -1, (255,0,255), 1)
 
 # draw point
-cv.circle(img, sleLT, 4, (0, 0, 255), -1)
-cv.circle(img, sleRT, 4, (0, 255, 0), -1)
 cv.circle(img, shirt.collar[0], 4, (255, 0, 0), -1)
-cv.circle(img, shirt.collar[1], 4, (255, 255, 0), -1)
+cv.circle(img, shirt.collar[1], 4, (255, 0, 0), -1)
+cv.circle(img, shirt.sleeveTop[0], 4, (0, 255, 0), -1)
+cv.circle(img, shirt.sleeveTop[1], 4, (0, 255, 0), -1)
+cv.circle(img, shirt.sleeveBottom[0], 4, (0, 255, 0), -1)
+cv.circle(img, shirt.sleeveBottom[1], 4, (0, 255, 0), -1)
 cv.circle(img, shirt.bodyBottom[0], 4, (127, 255, 0), -1)
 cv.circle(img, shirt.bodyBottom[1], 4, (127, 255, 0), -1)
+cv.circle(img, shirt.armHoleBottom[Tshirt.LEFT], 4, (0, 0, 255), -1)
+cv.circle(img, shirt.armHoleBottom[Tshirt.RIGHT], 4, (0, 0, 255), -1)
+
+# img = cv.drawContours(img, [cut], -1, (0,255,0), 1)
+# img = cv.drawContours(img, [huCut], -1, (0, 0, 255), 1)
 
 # show center of shirt
 shirt.printCenter(img, shirt.outline)
--- smart-shirt/shirtIP.py
+++ smart-shirt/shirtIP.py
@@ -4,37 +4,52 @@ import argparse
 import numpy as np
 import cv2 as cv
 
+LEFT = UPPER = 0
+RIGHT = UNDER = 1
+
+def plotContour(img, cont):
+    for point in cont:
+        cv.circle(img, tuple(point[0]), 2, (0, 0, 255), -1)
+
 class TShirt:
+    sleeveTop = []
     
     def __init__(self, fileName):
         self.imgPath = fileName
-    
+        # load the image and convert it to grayscale
+        self.img = cv.imread(self.imgPath)
+        # cv.imshow('original', img)
+
     def recognize(self):
+        # all get point function need call in order
         self.outline = self.getOutLineShirt()
-        self.hull = self.getHullShirt(self.outline)
+        self.hull = self.getHull(self.outline)
         self.ct = self.center(self.hull)
         self.collar = self.getCollar(self.hull, self.ct)
         self.bodyBottom = self.getBodyBottom(self.hull, self.ct)
+        self.sleeveTop.append(self.getsleeveTop(self.hull, side='left'))
+        self.sleeveTop.append(self.getsleeveTop(self.hull, side='right'))
+        self.armHoleBottom = self.getArmHole(self.outline)
+        # get 2 sleeve bottom points (left & right)
+        self.sleeveBottom = self.getSleeveBottom(self.outline)
+        # get 2 armhole top points (left & right)
+        self.armHoleTop = self.getArmHoleTop(self.outline)
 
     def getOutLineShirt(self):
-        # load the image and convert it to grayscale
-        img = cv.imread(self.imgPath)
-        # cv.imshow('original', img)
-
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
         # cv.imshow('gray', gray)
-        # thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 1)
-        (_, thresh) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY_INV)
+        thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 127, 1)
+        (_, thresh) = cv.threshold(gray, 145, 255, cv.THRESH_BINARY_INV)
         # cv.imshow('thresh', thresh)
 
         (_,conts,_) = cv.findContours(thresh,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         bigestCont = sorted(conts, key=cv.contourArea, reverse=True)[0]    
         return bigestCont
 
-    def getHullShirt(self, outline):
+    def getHull(self, outline):
         return cv.convexHull(outline)
 
-    def getSleeveUpper(self, hull, side='left', pos='top'):
+    def getsleeveTop(self, hull, side='left', pos='top'):
         arr = hull[:, 0]
         argX = arr[0][0]
         minY = arr[0][1]
@@ -57,6 +72,7 @@ class TShirt:
         return (argX, minY)
     
     def getBodyBottom(self, hull, ct):
+        "return two points of boddy bottom. poinst[0] is boddy bottom left and points[1] is boddy bottom right"
         arr = hull[:, 0]
         argXLeft = argXRight = ct[0]
         argYLeft = argYRight = ct[1]
@@ -73,6 +89,7 @@ class TShirt:
         return ((argXLeft, argYLeft), (argXRight, argYRight))        
 
     def getCollar(self, hull, ct):
+        "Return two points of collar. points[0] is point on the left and points[1] on the right"
         arr = hull[:, 0]
         lst = sorted(arr, key=lambda x: x[1])
         argX = lst[0][0]
@@ -82,8 +99,10 @@ class TShirt:
                 argX = point[0]
                 argY = point[1]
                 break
-
-        return (tuple(lst[0]), (argX, argY))
+        if(lst[0][0] < argX):
+            return (tuple(lst[0]), (argX, argY))
+        else:
+            return ((argX, argY), tuple(lst[0]))
 
     def center(self, outline):
         M = cv.moments(outline)
@@ -95,3 +114,256 @@ class TShirt:
         cv.circle(img, self.ct, 3, (255, 255, 255), -1)
         cv.putText(img, '('+str(self.ct[0])+','+str(self.ct[1])+')', (self.ct[0] + 20, self.ct[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
         return
+
+    def cutContour(self, outline, tail, head):
+        # return outline[:tail,:head]
+        s = slice(tail,head,1)
+        return outline[s]
+    
+    def getArmHole(self, outline):
+        otl = self.outline[:,0].tolist()
+        # find armhole at the left side
+        val1 = otl.index(list(self.sleeveTop[LEFT]))
+        val2 = otl.index(list(self.bodyBottom[LEFT]))
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutLeft = self.cutContour(self.outline, tail, head + 1)
+        hulCutLeft = self.getHull(cutLeft)
+
+        # cv.drawContours(self.img, [hulCutLeft], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+
+        arr = hulCutLeft[:, 0]
+        argXLeft = argYLeft = 0
+        k=True
+        for point in hulCutLeft:
+            if( (point[0][1]<self.ct[1]) & (point[0][0]>=(self.bodyBottom[LEFT][0])-20) ):
+                if(k):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+                    k = False
+                elif(argYLeft>point[0][1]):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+
+        # find armhole at the right side
+        val1 = otl.index(list(self.sleeveTop[RIGHT]))
+        val2 = otl.index(list(self.bodyBottom[RIGHT]))
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutRight = self.cutContour(self.outline, tail, head + 1)
+        hulCutRight = self.getHull(cutRight)
+
+        # cv.drawContours(self.img, [hulCutRight], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+        
+        arr = hulCutRight[:, 0]
+        argXRight = argYRight = 0
+        k=True
+        for point in hulCutRight:
+            if( (point[0][1]<self.ct[1]) & (point[0][0]<=(self.bodyBottom[RIGHT][0])+20) ):
+                if(k):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+                    k = False
+                elif(argYRight>point[0][1]):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+
+        return ((argXLeft, argYLeft), (argXRight, argYRight))
+
+    def getArmHole2(self, outline):
+        otl = self.outline[:,0].tolist()
+        # find armhole at the left side
+        val1 = otl.index(list(self.sleeveTop[LEFT]))
+        val2 = otl.index(list(self.bodyBottom[LEFT]))
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutLeft = self.cutContour(self.outline, tail, head + 1)
+        hulCutLeft = self.getHull(cutLeft)
+
+        # cv.drawContours(self.img, [hulCutLeft], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+
+        arr = hulCutLeft[:, 0]
+        argXLeft = argYLeft = 0
+        k=True
+        for point in hulCutLeft:
+            if( (point[0][1]<self.ct[1]) & (point[0][1]>(self.sleeveTop[LEFT][1]+10)) ):
+                if(k):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+                    k = False
+                elif(argYLeft>point[0][1]):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+
+        # find armhole at the right side
+        val1 = otl.index(list(self.sleeveTop[RIGHT]))
+        val2 = otl.index(list(self.bodyBottom[RIGHT]))
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutRight = self.cutContour(self.outline, tail, head + 1)
+        hulCutRight = self.getHull(cutRight)
+
+        # cv.drawContours(self.img, [hulCutRight], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+        
+        arr = hulCutRight[:, 0]
+        argXRight = argYRight = 0
+        k=True
+        for point in hulCutRight:
+            if( (point[0][1]<self.ct[1]) & (point[0][1]>(self.sleeveTop[RIGHT][1]+10)) ):
+                if(k):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+                    k = False
+                elif(argYRight>point[0][1]):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+
+        return ((argXLeft, argYLeft), (argXRight, argYRight))
+
+    def getSleeveBottom(self, outline):
+        '''Get two point of sleeve left (bottom point) and sleeve right (bottom point). 
+        This function need sleeve top points and armholeBottom have already found'''
+        otl = self.outline[:,0].tolist()
+        # find armhole at the left side
+        val1 = otl.index(list(self.sleeveTop[LEFT]))
+        val2 = otl.index(list(self.armHoleBottom[LEFT]))
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutLeft = self.cutContour(self.outline, tail, head + 1)
+        hulCutLeft = self.getHull(cutLeft)
+
+        # cv.drawContours(self.img, [hulCutLeft], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+        # plotContour(self.img, hulCutLeft)
+
+        arr = hulCutLeft[:, 0]
+        argXLeft = argYLeft = 0
+        k=True
+        for point in hulCutLeft:
+            # point will be between sleeveTop and armHoleBottom
+            if( (self.sleeveTop[LEFT][0]<point[0][0]) & (point[0][0]<self.armHoleBottom[LEFT][0]) ):
+                if(k):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+                    k = False
+                elif(argYLeft<point[0][1]):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+
+        # find armhole at the right side
+        val1 = otl.index(list(self.sleeveTop[RIGHT]))
+        val2 = otl.index(list(self.armHoleBottom[RIGHT]))
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutRight = self.cutContour(self.outline, tail, head + 1)
+        hulCutRight = self.getHull(cutRight)
+
+        # cv.drawContours(self.img, [hulCutRight], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+        # plotContour(self.img, hulCutRight)
+        # cv.imshow('haha', self.img)
+
+        arr = hulCutRight[:, 0]
+        argXRight = argYRight = 0
+        k=True
+        for point in hulCutRight:
+            # point will be between sleeveTop and armHoleBottom
+            if( (self.armHoleBottom[RIGHT][0]<point[0][0]) & (point[0][0]<self.sleeveTop[RIGHT][0]) ):
+                if(k):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+                    k = False
+                elif(argYRight<point[0][1]):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+
+        return ((argXLeft, argYLeft), (argXRight, argYRight))
+
+    def getArmHoleTop(self, outline):
+        '''Get two point of arm hole left (top point) and arm hole (top point). 
+        This function need sleeve top points and collar points have already found'''
+        otl = self.outline[:,0].tolist()
+        # find armhole at the left side
+        val1 = otl.index(list(self.sleeveTop[LEFT]))
+        val2 = otl.index(list(self.collar[LEFT]))
+        print(val1, val2)
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutLeft = self.cutContour(self.outline, tail, head + 1)
+        hulCutLeft = self.getHull(cutLeft)
+
+        cv.drawContours(self.img, [hulCutLeft], -1, (0, 0, 255), 1)
+        cv.imshow('haha', self.img)
+        plotContour(self.img, hulCutLeft)
+
+        arr = hulCutLeft[:, 0]
+        argXLeft = argYLeft = 0
+        k=True
+        for point in hulCutLeft:
+            # point will be between sleeveTop and armHoleBottom
+            if( (self.sleeveTop[LEFT][0]<point[0][0]) & (point[0][0]<self.armHoleBottom[LEFT][0]) ):
+                if(k):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+                    k = False
+                elif(argYLeft<point[0][1]):
+                    argYLeft = point[0][1]
+                    argXLeft = point[0][0]
+
+        # find armhole at the right side
+        val1 = otl.index(list(self.sleeveTop[RIGHT]))
+        val2 = otl.index(list(self.collar[RIGHT]))
+        print(val1, val2)
+        if(val1<val2):
+            tail = val1; head = val2
+        else:
+            tail = val2; head = val1
+
+        cutRight = self.cutContour(self.outline, tail, head + 1)
+        hulCutRight = self.getHull(cutRight)
+
+        # cv.drawContours(self.img, [hulCutRight], -1, (0, 0, 255), 1)
+        # cv.imshow('haha', self.img)
+        plotContour(self.img, hulCutRight)
+        cv.imshow('haha', self.img)
+
+        arr = hulCutRight[:, 0]
+        argXRight = argYRight = 0
+        k=True
+        for point in hulCutRight:
+            # point will be between sleeveTop and armHoleBottom
+            if( (self.armHoleBottom[RIGHT][0]<point[0][0]) & (point[0][0]<self.sleeveTop[RIGHT][0]) ):
+                if(k):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+                    k = False
+                elif(argYRight<point[0][1]):
+                    argYRight = point[0][1]
+                    argXRight = point[0][0]
+
+        return ((argXLeft, argYLeft), (argXRight, argYRight))
