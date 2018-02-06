a9133b69794338c661d8c028498336da734f667e
DuongNguyenHai <duongnguyenhai.hust@gmail.com>
2018-02-06 23:17:11 +0700

    suff thing

--- smart-shirt/stuff/client-image.js
+++ smart-shirt/stuff/client-image.js
@@ -3,12 +3,18 @@ example code from
 http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
 
 var net = require('net');
+var base64Img = require('base64-img');
 
 var client = new net.Socket();
 
 client.connect(8888, '127.0.0.1', function() {
     console.log('Connected to server !');
     process.stdout.write("message: ");
+    base64Img.base64('images/shirt15.jpg', function(err, data) {
+        client.write(data);
+        // client.write('/r');
+    })
+    
 });
 
 client.on('data', function(data) {
--- smart-shirt/stuff/tcp-server.js
+++ smart-shirt/stuff/tcp-server.js
@@ -3,9 +3,9 @@ var textChunk = '';
 var server = net.createServer(function(socket) {
 	socket.write('Echo server\r\n');
 	socket.on('data', function(data){
-		// console.log(data);
-		textChunk = data.toString('utf8');
-		console.log(textChunk);
+		console.log(data);
+		// textChunk = data.toString('utf8');
+		// console.log(textChunk);
 		// socket.write(textChunk);
 	});
 });
